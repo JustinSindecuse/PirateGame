a172d782c9eae1db72cc26724137f672c56bbc83
Justin Sindecuse-Hayden <sindecus@usc.edu>
2020-02-01 12:53:33 -0800

    Initial commit with CannonStuff and PlayerController

---
+++ CannonStuff.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonStuff : MonoBehaviour
+{
+    bool opCannon;
+    string objName;
+
+    void Start()
+    {
+        opCannon = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D dataFromCollision)
+    {
+        Debug.Log("OnCollisionEnter2D");
+        string name = dataFromCollision.gameObject.name;
+        if(name == "Pirate")
+        {
+            objName = name;
+            opCannon = true;
+        }
+        
+    }
+
+    void Update()
+    {
+        if (opCannon == true)
+        {
+            //Rotating and firing cannon
+            if (Input.GetKey("a"))
+            {
+                Vector3 temp = new Vector3(0, 0, 3);
+                gameObject.transform.Rotate(temp, Space.Self);
+            }
+        }
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Animator animator;
+    public bool opCannon;
+    public string cannonName;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        opCannon = false;
+        cannonName = "";
+    }
+
+    void OnCollisionEnter2D(Collision2D dataFromCollision)
+    {
+        //Debug.Log("OnCollisionEnter2D");
+        string name = dataFromCollision.gameObject.name;
+        if (name == "Cannon1" || name == "Cannon2" ||
+            name == "Cannon3" || name == "Cannon4" ||
+            name == "Cannon5")
+        {
+            opCannon = true;
+            cannonName = name;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (opCannon == true)
+        {
+            //Breaks cannon operation
+            if (Input.GetKey("c"))
+            {
+                opCannon = false;
+                cannonName = "";
+            }
+            //Lock player position to cannon
+            if (cannonName == "Cannon4")
+            {
+                Vector3 newPos = new Vector3(.31f, -.76f, 0);
+                transform.position = newPos;
+            }
+            else if (cannonName == "Cannon5")
+            {
+                Vector3 newPos = new Vector3(-4.41f, .39f, 0);
+                transform.position = newPos;
+            }
+            else if(cannonName == "Canon3")
+            {
+                Vector3 newPos = new Vector3(.31f, 1.09f, 0);
+                transform.position = newPos;
+            }
+            else if(cannonName == "Cannon2")
+            {
+                Vector3 newPos = new Vector3(1.77f, 1.22f, 0);
+                transform.position = newPos;
+            }
+            else if(cannonName == "Cannon1")
+            {
+                Vector3 newPos = new Vector3(1.71f, -.42f, 0);
+                transform.position = newPos;
+            }
+            
+        }
+        else
+        {
+            Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            moveVelocity = moveInput.normalized * speed;
+            if (Input.GetAxis("Vertical") > 0)
+            {
+                animator.SetFloat("VerticalSpeed", 1);
+            }
+            else if (Input.GetAxis("Vertical") < 0)
+            {
+                animator.SetFloat("VerticalSpeed", -1);
+            }
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                if (Input.GetAxis("Vertical") == 0)
+                {
+                    animator.SetFloat("HorizontalSpeed", 1);
+                }
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                if (Input.GetAxis("Vertical") == 0)
+                {
+                    animator.SetFloat("HorizontalSpeed", -1);
+                }
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        animator.SetFloat("VerticalSpeed", 0);
+        animator.SetFloat("HorizontalSpeed", 0);
+    }
+}
